3016c6bc83910f47f02d5d516f781a0857c713d5
jisu <ddc02336@gmail.com>
2025-08-17 17:56:25 +0900

    mb-nav 스크롤 문제, 바탕 body 스크롤 문제, body클릭시 mb-nav닫히게 하기 해결!

--- js/mb-nav.js
+++ js/mb-nav.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  // 메뉴바 클릭
   $("#mb-menuBar").click(function () {
     $(".mb-nav").animate(
       {
@@ -7,7 +6,28 @@ $(document).ready(function () {
       },
       400
     );
+    $("#overlay").addClass("active");
+    $("body").addClass("no-scroll");
   });
+
+  $("#overlay, #close-menu").click(function () {
+    $(".mb-nav").animate({
+      left: "-400px",
+    });
+    $("#overlay").removeClass("active");
+    $("body").removeClass("no-scroll");
+  });
+  // 메뉴바 클릭
+  // $("#mb-menuBar").click(function () {
+  //   $(".mb-nav").animate(
+  //     {
+  //       left: 0,
+  //     },
+  //     400
+  //   );
+  //   $("body").addClass("dimmed");
+  // });
+
   // 모바일용 메뉴 클릭시
   $(".mb-nav .gnb > li > a").click(function () {
     console.log(this);
@@ -15,9 +35,10 @@ $(document).ready(function () {
     $(this).next(".mb-nav .mb-depth2").stop().slideToggle();
   });
   // 모바일용 메뉴 닫기 버튼
-  $("#close-menu").click(function () {
-    $(".mb-nav").animate({
-      left: "-400px", //메뉴가 왼쪽 밖으로 나가도록 한다.
-    });
-  });
+  // $("#close-menu").click(function () {
+  //   $(".mb-nav").animate({
+  //     left: "-400px", //메뉴가 왼쪽 밖으로 나가도록 한다.
+  //   });
+  //   $("body").removeClass("dimmed");
+  // });
 });
